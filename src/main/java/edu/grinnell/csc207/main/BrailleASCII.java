501244720f2d4c7aa8f42bcc930e9b26240279ab
cyphersa <cyphersa@grinnell.edu>
2024-12-17 18:10:54 -0600

    Printing random unicode in main

--- src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
+++ src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
@@ -18,23 +18,40 @@ public class BrailleASCII {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     String target = "";
-    
-    for (int i = 0; i < args.length; i++) {
-      if (i == 0) {
-        target = args[i];
-      } else {
-        char[] letters = args[i].toCharArray();
-        if (target.equals("braille")) {
-          for (int j = 0; j < letters.length; j++) {
-            pen.print(BrailleAsciiTables.toBraille(letters[j]));
-          }
-        } else if (target.equals("ascii")) {
-          for (int j = 0; j < letters.length; j++) {
-            pen.print(BrailleAsciiTables.toBraille(letters[j]));
+    try {
+      for (int i = 0; i < args.length; i++) {
+        if (i == 0) {
+          target = args[i];
+        } else {
+          if (target.equals("braille")) {
+            char[] letters = args[i].toCharArray();
+            for (int j = 0; j < letters.length; j++) {
+              pen.print(BrailleAsciiTables.toBraille(letters[j]));
+            } // for-loop
+          } else if (target.equals("ascii")) {
+            if (args[i].length() % 6 != 0) {
+              throw new Exception("Invalid Length");
+            } // if
+
+            for (int j = 0; j < args[i].length(); j+= 6) {
+              String bits = args[i].substring(j, j + 6);
+              pen.print(BrailleAsciiTables.toAscii(bits));
+            } // for-loop
+          } else if (target.equals("unicode")) {
+            char[] letters = args[i].toCharArray();
+            for (int j = 0; j < letters.length; j++) {
+              String braille = BrailleAsciiTables.toBraille(letters[j]);
+              String unicode = BrailleAsciiTables.toUnicode(braille);
+              char h2u = (char) Integer.parseInt(unicode);
+              pen.print(h2u);
+            } // for-loop
           }
         }
       }
-    }
+    } catch (Exception e) {
+      pen.print("Runtime Exception");
+    } // try/catch
+    pen.println();
     pen.close();
   } // main(String[]
 
